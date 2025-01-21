11c04c4e67bf5ee41c905e8edc2e565d51a7a726
Sukhpreet-Singh2428 <sukhpreetsingh3910a@gmail.com>
2025-01-21 12:30:44 +0530

    Solution to Check LSB of a Number

--- Check LSB of a Number@@67882a731b4392d8fc1577e8/code.c
+++ Check LSB of a Number@@67882a731b4392d8fc1577e8/code.c
@@ -2,7 +2,7 @@
 int main() {
     int a;
     scanf("%d", &a);
-    if(a&1==1) {
+    if(a&1==1) {                    //if number is odd: 1(set) or if number is even: 0(Not set). so we can do it also by checking odd even numbers
         printf("Set");
     } else{
         printf("Not Set");
