86f80925d41f052caa61b10cabdcc3173a840043
Sukhpreet-Singh2428 <sukhpreetsingh3910a@gmail.com>
2025-01-23 13:46:29 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main() {
-    int x,y;
+    float x,y;
     char z;
-    scanf("%d %d %c", &x,&y,&z);
+    scanf("%f %f %c", &x,&y,&z);
     if(z=='+') {
         printf("%d", x+y);
     } else if(z=='-') {
