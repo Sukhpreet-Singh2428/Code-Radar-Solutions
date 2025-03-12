cd5341842378da355d587bec524a9cafb0640178
Sukhpreet-Singh2428 <sukhpreetsingh3910a@gmail.com>
2025-03-12 14:46:18 +0530

    Solution to Move All Zeros to the End of an Array

--- Move All Zeros to the End of an Array@@676c02002dbc8c3e7a280b7e/code.c
+++ Move All Zeros to the End of an Array@@676c02002dbc8c3e7a280b7e/code.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
-void swap(int* a,int* b){
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+void swap(int a,int b){
+    int temp = a;
+    a = b;
+    b = temp;
     return;
 }
 int main(){
