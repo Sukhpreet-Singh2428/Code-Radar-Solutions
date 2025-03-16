e78f12608baf0d304b2dbc74088bcddb10c75202
Sukhpreet-Singh2428 <sukhpreetsingh3910a@gmail.com>
2025-03-16 12:19:36 +0530

    Solution to Kth Missing Positive Number

--- Kth Missing Positive Number@@67ca7acd1fe08d7207c253c5/code.c
+++ Kth Missing Positive Number@@67ca7acd1fe08d7207c253c5/code.c
@@ -1,6 +1,6 @@
 int findKthMissing(int arr[],int n,int k){
     int smallest = arr[0];  // smallest positive number possible is 1 but in this question we have to tell at particular kth element.
-    for(int i=0;i<k-1;i++){
+    for(int i=0;i<=k-1;i++){
         smallest++;
     }
     return smallest;
