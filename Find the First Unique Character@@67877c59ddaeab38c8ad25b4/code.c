76d0acfea76ce3af71f545eccc1c45e95565d1e2
Sukhpreet-Singh2428 <sukhpreetsingh3910a@gmail.com>
2025-04-07 21:40:53 +0530

    Solution to Find the First Unique Character

---
+++ Find the First Unique Character@@67877c59ddaeab38c8ad25b4/code.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+char firstNonRepeated(char* str, int len){
+    for(int i=0;i<len;i++){
+        int flag = 1;
+        for(int j=0;j<len;j++){
+            if(i!=j && str[i]==str[j]){
+                flag = 0;
+                break;
+            }
+        }
+        if(flag){
+            return str[i];
+        }
+    }
+    return '-';
+}
+int main(){
+    char str[40];
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str,"\n")] = '\0';
+    int len = strlen(str);
+    char result = firstNonRepeated(str,len);
+    printf("%c",result);
+    return 0;
+}
