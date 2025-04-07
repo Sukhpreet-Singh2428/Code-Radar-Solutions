8479ce41f8021c327413f084b6f33aecbf9d920e
Sukhpreet-Singh2428 <sukhpreetsingh3910a@gmail.com>
2025-04-07 21:28:53 +0530

    Solution to Find the Largest Palindromic Substring

--- Find the Largest Palindromic Substring@@67877c59ddaeab38c8ad25af/code.c
+++ Find the Largest Palindromic Substring@@67877c59ddaeab38c8ad25af/code.c
@@ -1,32 +1,29 @@
-#include<stdio.h>
-#include<string.h>
-void reverse(char* str1, int len){
-    int i = 0;
-    int j = len-1;
-    while(i<=j){
-        char temp = str1[i];
-        str1[i] = str1[j];
-        str1[j] = temp;
+#include <stdio.h>
+#include <string.h>
+int isPalindrome(char* str, int start, int end) {
+    while(start < end) {
+        if(str[start] != str[end]) return 0;
+        start++;
+        end--;
+    }
+    return 1;
+}
+int main() {
+    char str[100];
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0';  
+    int len = strlen(str);
+    int i = 0, j = len - 1;
+    // Trim matching characters from both ends
+    while(i < j && str[i] == str[j]) {
         i++;
         j--;
     }
-}
-int main(){
-    char str[40];
-    fgets(str, sizeof(str), stdin);
-    str[strcspn(str,"\n")] = '\0';
-    char str1[40];
-    strcpy(str1,str);
-    int len = strlen(str);
-    reverse(str1,len);
-    int i = 0;
-    int j = len-1;
-    while(i<len && str[i]==str1[i]) i++;
-    while(j>=0 && str[j]==str1[j]) j--;
-    if(i<=j){
-        for(int k=i;k<=j;k++){
-        printf("%c",str[k]);
+    // Now check if the middle part is already a palindrome
+    if(isPalindrome(str, i, j)) {
+        for(int k = i; k <= j; k++) {
+            printf("%c", str[k]);
         }
-    }
+    } 
     return 0;
 }
